c5d4b9f743cbd41eed277703f7edf8f5430cd964
DaryaVin <DaryaVin1995@gmail.com>
2022-08-09 16:17:35 +0700

    Chenged checkboxButton component
    Minor changes:
    1. Changed the way of accessing and the type of the variable "children"

--- src/components/checkboxButton/checkboxButton.tsx
+++ src/components/checkboxButton/checkboxButton.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import "./checkboxButton.scss";
 
 interface CheckboxButtonProps extends  React.InputHTMLAttributes<HTMLInputElement>{
-  label: string,
+  label?: string,
   // children?: null,
+  children?:  React.ReactNode,
   explanation?: string,
   theme?: "withExplanation",
   type?: "checkbox",
   className?: string,
 }
-export const CheckboxButton = ({label, explanation, theme, className, ...props}: CheckboxButtonProps) => {
+export const CheckboxButton = ({label, children, explanation, theme, className, ...props}: CheckboxButtonProps) => {
 
   return (
     <label className={"checkboxButton" + (theme ? " checkboxButton_theme_" + theme  : "") + (className ? " " + className : "")}>
@@ -20,8 +21,8 @@ export const CheckboxButton = ({label, explanation, theme, className, ...props}:
         </div>
       </div>
       <div className={"checkboxButton__label"}>
-        {label}
-        {props.children}
+        {label + " "} 
+        {children}
         {
           theme === "withExplanation" 
           && (<p className="checkboxButton__explanation">{explanation}</p>)

5a1a3d4e3807bad7959650ab3244711812e2e89c
DaryaVin <DaryaVin@gmail.com>
2023-03-25 21:59:16 +0700

    Update appRouter component
    Implemented update of user data when authorization is changed

--- src/components/appRouter/appRouter.tsx
+++ src/components/appRouter/appRouter.tsx
@@ -3,22 +3,32 @@ import { connect, ConnectedProps } from 'react-redux';
 import { Routes } from 'react-router-dom';
 import { routes } from '../../routes';
 import { routesParser } from '../routesParser/routesParser';
-import { CheckAuth } from "../../store/actions/authActions";
+import { CheckAuth, FetchUserInfo } from "../../store/actions/authActions";
+import { RootState } from '../../store/reducers/rootReducer';
 
-const AppRout = ({ CheckAuth }: ConnectorProps) => {
+const AppRout = ({ auth, CheckAuth, FetchUserInfo }: ConnectorProps) => {
   useEffect(() => {
-      CheckAuth();
+    CheckAuth();
   });
+  useEffect(() => {
+    FetchUserInfo(auth?.uid);
+    console.log(auth);
+  }, [auth]);
   return (
     <Routes>
       {routesParser(routes)}
     </Routes>
   )
 }
-
+const mapStateToProps = (state: RootState) => {
+  return ({
+    auth: state.auth?.auth,
+  })
+}
 const mapDispatchToProps = {
-  CheckAuth
+  CheckAuth,
+  FetchUserInfo,
 };
-const connector = connect(null, mapDispatchToProps);
+const connector = connect(mapStateToProps, mapDispatchToProps);
 type ConnectorProps = ConnectedProps<typeof connector>;
 export const AppRouter = connector(AppRout);

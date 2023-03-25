bb10c32d5a5f28adc03e1fc6663ea267909b520d
DaryaVin <DaryaVin@gmail.com>
2023-03-25 22:11:13 +0700

    Header component made responsive
    Burger menu method implemented

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import "./header.scss";
 import { connect, ConnectedProps } from 'react-redux';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation, useNavigate } from 'react-router-dom';
 import { RootState } from '../../store/reducers/rootReducer';
 import { Button } from '../button/button';
 import { FlexContainer } from '../flexContainer/flexContainer';
 import { Logo } from '../logo/logo';
 
 export const Head = ({ auth, userInfo }: ConnectorProps) => {
+  let location = useLocation();
+
+  useEffect(() => {
+    setshowMenu(false);
+  }, [location]);
   const navLinks = [
     {
       path: "/",
@@ -19,66 +24,90 @@ export const Head = ({ auth, userInfo }: ConnectorProps) => {
       title: "Выбор номера"
     },
   ];
+  const [showMenu, setshowMenu] = useState<boolean>(false);
   return (
-    <header>
-      <FlexContainer key={"header__wrap"}
-        className={"header__wrap"}
-        alignItems='center'
-        justifyContent={"space-between"}
-      >
-        <NavLink key={"header__logo"}
-          to="/"
-          className='header__logo'
+
+    <header className='header'>
+      <div className="header__flowOccupyingPart"></div>
+      <div className="header__fixedPart">
+        <div key={"header__wrap"}
+          className={"header__wrap"}
         >
-          <Logo />
-        </NavLink>
-        <FlexContainer key={"header__navbar"}
-          className={"header__navbar"}
-          justifyContent="end"
-          tagForWrap='nav'
-          flexWrap="nowrap"
-        >
-          <FlexContainer key={"header__navbarList"}
-            className='header__navbarList'
-            tagForWrap='ul'
-            justifyContent="space-around"
-            columnGap={30}
-            flexWrap="wrap"
+          <NavLink key={"header__logo"}
+            to="/"
+            className='header__logo'
           >
-            {
-              navLinks.map((item, index) => <NavLink key={index} to={item.path} end={item.end} className={"header__navbarItem"}>{item.title}</NavLink>)
-            }
-          </FlexContainer>
-          {
-            auth && userInfo
-              ? <NavLink key={"header__authBlock"}
-                to={"/profile"}
-                className="header__authBlock header__authBlock_auth"
-              >
-                {userInfo.lastName} {userInfo.firstName}
-              </NavLink>
-              : <FlexContainer key={"header__authBlock"}
+            <Logo />
+          </NavLink>
+          <nav key={"header__navbar"}
+            className={"header__navbar" + (showMenu ? " show" : "")}
+            onScroll={(e) => { e.preventDefault(); e.stopPropagation(); }}
+          >
+            <FlexContainer key={"header__navbarScroll"}
+              className="header__navbarScroll"
+              justifyContent="space-between"
+            >
+              <div></div>
+              <FlexContainer key={"header__navbarList"}
+                className='header__navbarList'
                 tagForWrap='ul'
-                justifyContent="space-between"
                 alignItems='center'
-                className="header__authBlock"
-                columnGap={20}
+                columnGap={30}
+                flexWrap="nowrap"
               >
-                <li>
-                  <Button theme="withBorder">
-                    <NavLink to="/login">войти</NavLink>
-                  </Button>
-                </li>
-                <li>
-                  <Button theme="fillBcg">
-                    <NavLink to="/login">Зарегистрироваться</NavLink>
-                  </Button>
-                </li>
+                {
+                  navLinks.map((item, index) =>
+                    <li key={index} className={"header__navbarItem"}>
+                      <FlexContainer
+                        justifyContent="space-between"
+                        alignItems='center'
+                      >
+                        <NavLink to={item.path} end={item.end} className={"header__navbarLink"}>{item.title}</NavLink>
+                      </FlexContainer>
+                    </li>
+                  )
+                }
               </FlexContainer>
-          }
-        </FlexContainer>
-      </FlexContainer>
-    </header>
+            </FlexContainer>
+            {
+              auth && userInfo
+                ? <NavLink key={"header__authBlock"}
+                  to={"/profile"}
+                  className="header__authBlock header__authBlock_auth"
+                >
+                  {
+                  userInfo.lastName || userInfo.firstName
+                  ? userInfo.lastName + " " + userInfo.firstName
+                  : "Не известный"
+                }
+                </NavLink>
+                : <FlexContainer key={"header__authBlock"}
+                  tagForWrap='ul'
+                  justifyContent="space-between"
+                  alignItems='center'
+                  className="header__authBlock"
+                  columnGap={20}
+                >
+                  <li>
+                    <Button theme="withBorder">
+                      <NavLink to="/login">войти</NavLink>
+                    </Button>
+                  </li>
+                  <li>
+                    <Button theme="fillBcg">
+                      <NavLink to="/login">Зарегистрироваться</NavLink>
+                    </Button>
+                  </li>
+                </FlexContainer>
+            }
+          </nav>
+          <button key={"header__burger"} type='button' className={"header__burger" + (showMenu ? " show" : "")} onClick={() => setshowMenu(!showMenu)}>
+            Навигация
+            <span></span>
+          </button>
+        </div>
+      </div>
+    </header >
   )
 }
 

6d429a6caae06fcdd4c51d4de79634cba1694d30
DaryaVin <DaryaVin@gmail.com>
2023-04-06 14:49:55 +0700

    The Carousel component is changed.
    Added the ability to pass additional parameters to the main div of the component. The situation of missing content has been handled.

--- src/components/carousel/carousel.tsx
+++ src/components/carousel/carousel.tsx
@@ -2,23 +2,32 @@ import React, { Children, useEffect, useState } from "react";
 import { CreateWrapElement, WrapElementContentType } from "../createWrapElement/createWrapElement";
 import "./carousel.scss";
 
-interface CarouselProps {
-  children: WrapElementContentType,
+interface CarouselProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+  children: WrapElementContentType | undefined,
   width: number,
   height: number,
   className?: string,
 }
-export const Carousel = ({ children, width, height, className }: CarouselProps) => {
+export const Carousel = ({ children, width, height, className, ...props }: CarouselProps) => {
   let [contentState, setContentState] = useState<JSX.Element[]>([]);
   let [currantItemState, setCurrantItemState] = useState<number>(0);
+
   useEffect(() => {
-    setContentState(
-      Children.map(children, (child => {
-        return <CreateWrapElement className="carousel__contentItem">
-          {child}
-        </CreateWrapElement>
-      }))
-    );
+    if (children) {
+      setContentState(
+        Children.map(children, ((child) => {
+          return <CreateWrapElement className="carousel__contentItem">
+            {child}
+          </CreateWrapElement>
+        }))
+      );
+    } else {
+      setContentState([
+        <div className="carousel__contentItem">
+          Изображение отсутствует
+        </div>
+      ])
+    }
   }, [])
   const OnClickPrev = () => {
     setCurrantItemState(Math.max(currantItemState - 1, 0));
@@ -28,6 +37,7 @@ export const Carousel = ({ children, width, height, className }: CarouselProps) 
   }
   return (
     <div
+      {...props}
       className={className ? className + " carousel" : "carousel"}
       style={{
         width: width,
@@ -35,36 +45,43 @@ export const Carousel = ({ children, width, height, className }: CarouselProps) 
       }}
     >
       <div className="carousel__wrap">
-        <div
+        <div key={"carousel__contentContener"}
           className="carousel__contentContener"
           style={{
             transform: `translateX(${-(currantItemState * width)}px)`,
           }}
         >
-          {contentState}
+          <CreateWrapElement style={{
+            width: width,
+            height: height,
+          }}>
+            {contentState}
+          </CreateWrapElement>
         </div>
-        <button type="button" className="carousel__button carousel__buttonPrev" onClick={OnClickPrev}>&#60;</button>
-        <button type="button" className="carousel__button carousel__buttonNext" onClick={onClickNext}>&#62;</button>
+        <button key={"carousel__buttonPrev"} type="button" className="carousel__button carousel__buttonPrev" onClick={OnClickPrev}>&#60;</button>
+        <button key={"carousel__buttonNext"} type="button" className="carousel__button carousel__buttonNext" onClick={onClickNext}>&#62;</button>
         {
           contentState.length <= 10 &&
-          <ul className="carousel__dotsList">
+          <ul key={"carousel__dotsList"} className="carousel__dotsList">
             {
-              contentState.map((item, index) => {
-                return (
-                  <li
-                    tabIndex={0}
-                    onClick={() => { setCurrantItemState(index) }}
-                    onKeyDown={(e) => { if (e.code === "Enter") setCurrantItemState(index) }}
-                    key={index}
-                    className={
-                      index === currantItemState
-                        ? "carousel__dotsItem carousel__dotsItem_current"
-                        : "carousel__dotsItem"
-                    }
-                  >
-                  </li>
-                )
-              })
+              contentState.length > 1
+                ? contentState.map((item, index) => {
+                  return (
+                    <li
+                      tabIndex={0}
+                      onClick={() => { setCurrantItemState(index) }}
+                      onKeyDown={(e) => { if (e.code === "Enter") setCurrantItemState(index) }}
+                      key={index}
+                      className={
+                        index === currantItemState
+                          ? "carousel__dotsItem carousel__dotsItem_current"
+                          : "carousel__dotsItem"
+                      }
+                    >
+                    </li>
+                  )
+                })
+                : ""
             }
           </ul>
         }

8a0619f7c06b12c660385b2969923463b2a14355
DaryaVin <31697627+DaryaVin@users.noreply.github.com>
2023-08-22 17:39:51 +0700

    The review component chenged.

--- src/components/review/review.tsx
+++ src/components/review/review.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 import "./review.scss";
 import { LikeButton } from "../likeButton/likeButton";
 import { CreateWrapElement } from "../createWrapElement/createWrapElement";
+import { ReviewItem } from "../../types/rooms";
+import { RootState } from "../../store/reducers/rootReducer";
+import { ConnectedProps, connect } from "react-redux";
+import { RoomsAPI } from "../../interfaces/roomsAPI";
 
 const useDateFormat = (date: Date, reportDate: Date): string => {
   const oneDay = 1000 * 60 * 60 * 24;
@@ -24,6 +28,7 @@ const useDateFormat = (date: Date, reportDate: Date): string => {
       default: return date.toLocaleString("Ru-ru", { day: "numeric", month: 'short' });
     }
   }
+
   if (reportDate <= date && date.getTime() - reportDate.getTime() < 8 * oneDay) {
     const date1 = new Date(date.getTime());
     const reportDate1 = new Date(reportDate.getTime());
@@ -48,28 +53,41 @@ const useDateFormat = (date: Date, reportDate: Date): string => {
   return date.toLocaleString("Ru-ru", { day: "numeric", month: 'short', year: 'numeric' }).slice(0, -2);
 }
 
-interface ReviewProps {
-  id: string,
-  authorName: string,
-  dateToCreating: Date,
-  content: string,
-  listWhoLikedThisReview: string[],
-  authorPhoto?: JSX.Element,
-  className?: string,
-}
-export const Review = ({
+type ReviewProps = {
+  roomId: string | number,
+} 
+& ReviewItem 
+& ConnectorProps 
+& React.HTMLAttributes<HTMLDivElement>;
+export const RevItem = ({
+  roomId,
   id,
+  authorId,
   authorName,
   dateToCreating,
   content,
   listWhoLikedThisReview,
-  authorPhoto,
-  className,
+  authorPhotoURL,
+  auth,
+  ...props
 }: ReviewProps) => {
+  const [newListWhoLikedThisReview, setNewListWhoLikedThisReview]= useState<string[]>(listWhoLikedThisReview ? listWhoLikedThisReview : [])
+  const onClickLikeButton = async () => { 
+    if (auth) {
+      const newListWhoLikedThisReviewItem = await RoomsAPI.FetchLikeReviews(auth.uid, roomId, id, newListWhoLikedThisReview);
+      console.log("onClickLikeButton newListWhoLikedThisReviewItem", newListWhoLikedThisReviewItem);
+      
+      setNewListWhoLikedThisReview(newListWhoLikedThisReviewItem);
+    }
+   }
   return (
-    <div className={className ? className + " review" : "review"}>
+    <div {...props} className={props.className ? props.className + " review" : "review"}>
       <CreateWrapElement className="review__photo">
-        {authorPhoto || "Фото нет"}
+        {
+          authorPhotoURL
+            ? <img src={authorPhotoURL} alt="Фото автора отзыва" />
+            : "Фото нет"
+        }
       </CreateWrapElement>
       <div className="review__info">
         <div className="review__authorName">
@@ -80,7 +98,11 @@ export const Review = ({
         </div>
       </div>
       <div className="review__likeButton">
-        <LikeButton checked={true} numberLikes={listWhoLikedThisReview.length}></LikeButton>
+        <LikeButton
+          checked={auth ? newListWhoLikedThisReview.includes(auth.uid) : false}
+          numberLikes={newListWhoLikedThisReview.length}
+          onClick={onClickLikeButton}
+        />
       </div>
       <div className="review__content">
         {content}
@@ -90,3 +112,20 @@ export const Review = ({
 
   )
 }
+
+const mapStateToProps = (state: RootState) => {
+  return ({
+    auth: state.auth?.auth,
+    // userInfo: state.auth?.userInfo,
+    // settings: state.filterRooms?.settings,
+    // designations: state.filterRooms?.designations,
+    // isLoading: state.filterRooms?.isLoading
+  })
+}
+const mapDispatchToProps = {
+  // FetchDesignations,
+};
+
+const connector = connect(mapStateToProps, mapDispatchToProps);
+type ConnectorProps = ConnectedProps<typeof connector>;
+export const Review = connector(RevItem);

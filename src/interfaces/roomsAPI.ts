8608a3e3edfd77cbc02e09db611d82145b534fd8
DaryaVin <31697627+DaryaVin@users.noreply.github.com>
2023-08-22 17:34:01 +0700

    New methods have been added to the roomAPI to load data for a specific room and to update some data in reviews.

--- src/interfaces/roomsAPI.ts
+++ src/interfaces/roomsAPI.ts
@@ -1,7 +1,8 @@
-import { child, get, ref } from "firebase/database";
+import { child, get, ref, update } from "firebase/database";
 import { bdFirebase } from "../firebase";
 import { settings } from "../types/filterRooms";
-import { RoomItem } from "../types/rooms";
+import { ReviewItem, RoomItem } from "../types/rooms";
+import { authAPI } from "./authAPI";
 
 export const RoomsAPI = {
   FetchRooms: async (filterSettings: settings, pageSize: number, currentPage: number) => {
@@ -9,8 +10,18 @@ export const RoomsAPI = {
     const rooms = await get(child(dbRef, `rooms`)).then((item) => {
       const rooms: any[] = Object.values(JSON.parse(JSON.stringify(item)));
       return rooms.map((roomItem) => {
+        let reviews: ReviewItem[] | undefined = roomItem.reviews ? Object.values(roomItem.reviews) as ReviewItem[] : undefined;
+        reviews = reviews?.map((review) => {
+          return {
+            ...review,
+            dateToCreating: new Date(review.dateToCreating),
+          }
+        });
+        let photos: string[] | undefined = roomItem.photos ? Object.values(roomItem.photos) : undefined;
         const room: RoomItem = {
           ...roomItem,
+          reviews: reviews,
+          photos,
           roomConditions: {
             ...roomItem.roomConditions,
             roomRules: roomItem.roomConditions.roomRules ? Object.values(roomItem.roomConditions.roomRules) : roomItem.roomConditions.roomRules,
@@ -54,13 +65,13 @@ export const RoomsAPI = {
     // console.log("RoomsAPI",rooms);
 
     const roomsItems: RoomItem[] = Object.values(JSON.parse(JSON.stringify(rooms)));
-    let roomsItemsWithPagination:  RoomItem[] = [];
+    let roomsItemsWithPagination: RoomItem[] = [];
     const newCurrentPage = pageSize * (currentPage - 1) > roomsItems.length ? 1 : currentPage;
     for (let index = pageSize * (newCurrentPage - 1); index < pageSize * newCurrentPage && index < roomsItems.length; index++) {
       roomsItemsWithPagination.push(roomsItems[index]);
     }
 
-    return{
+    return {
       rooms: roomsItemsWithPagination,
       totalCount: roomsItems.length,
       newCurrentPage,
@@ -70,16 +81,48 @@ export const RoomsAPI = {
     const dbRef = await ref(bdFirebase);
     const roomItem: RoomItem = await get(child(dbRef, `rooms/` + roomId)).then((item) => {
       const roomItem: RoomItem = JSON.parse(JSON.stringify(item));
-      return {
-          ...roomItem,
-          roomConditions: {
-            ...roomItem.roomConditions,
-            roomRules: roomItem.roomConditions.roomRules ? Object.values(roomItem.roomConditions.roomRules) : roomItem.roomConditions.roomRules,
-            facilitiesInRoom: roomItem.roomConditions.facilitiesInRoom ? Object.values(roomItem.roomConditions.facilitiesInRoom) : roomItem.roomConditions.facilitiesInRoom,
-            equipmentInRoom: roomItem.roomConditions.equipmentInRoom ? Object.values(roomItem.roomConditions.equipmentInRoom) : roomItem.roomConditions.equipmentInRoom,
-          }
+      let reviews: ReviewItem[] | undefined = roomItem.reviews ? Object.values(roomItem.reviews) as ReviewItem[] : undefined;
+      reviews = reviews?.map((review) => {
+        let listWhoLikedThisReview = review.listWhoLikedThisReview ? Object.values(review.listWhoLikedThisReview) : undefined;
+        return {
+          ...review,
+          dateToCreating: new Date(review.dateToCreating),
+          listWhoLikedThisReview: listWhoLikedThisReview ? listWhoLikedThisReview : [],
         }
       })
+      let photos: string[] | undefined = roomItem.photos ? Object.values(roomItem.photos) : undefined;
+
+      return {
+        ...roomItem,
+        reviews,
+        photos,
+        roomConditions: {
+          ...roomItem.roomConditions,
+          roomRules: roomItem.roomConditions.roomRules ? Object.values(roomItem.roomConditions.roomRules) : roomItem.roomConditions.roomRules,
+          facilitiesInRoom: roomItem.roomConditions.facilitiesInRoom ? Object.values(roomItem.roomConditions.facilitiesInRoom) : roomItem.roomConditions.facilitiesInRoom,
+          equipmentInRoom: roomItem.roomConditions.equipmentInRoom ? Object.values(roomItem.roomConditions.equipmentInRoom) : roomItem.roomConditions.equipmentInRoom,
+        }
+      }
+    })
     return roomItem;
-   }
+  },
+  FetchLikeReviews: async (userId: string, roomId: string | number, reviewId: string, listWhoLikedThisReview: string[]) => {
+    const dbRef = await ref(bdFirebase);
+    let newListWhoLikedThisReview: string[] = [];
+    if (listWhoLikedThisReview.includes(userId)) {
+      const index = listWhoLikedThisReview.findIndex((item) => {
+        return item === userId;
+      })
+      newListWhoLikedThisReview = [...listWhoLikedThisReview];
+      newListWhoLikedThisReview.splice(index, 1);
+      console.log("FetchLikeReviews newListWhoLikedThisReview", newListWhoLikedThisReview);
+      
+      await update(dbRef, { [`rooms/${roomId}/reviews/${reviewId}/listWhoLikedThisReview`]: newListWhoLikedThisReview });
+    } else {
+      newListWhoLikedThisReview = [...listWhoLikedThisReview, userId];
+      console.log("FetchLikeReviews newListWhoLikedThisReview", newListWhoLikedThisReview);
+      await update(dbRef, { [`rooms/${roomId}/reviews/${reviewId}/listWhoLikedThisReview`]: newListWhoLikedThisReview });
+    }
+    return newListWhoLikedThisReview;
+  }
 }

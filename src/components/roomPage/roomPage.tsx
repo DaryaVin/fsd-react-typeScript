c5e5d21232be535aaa817da5405e86a3d91eb62b
DaryaVin <31697627+DaryaVin@users.noreply.github.com>
2023-09-14 16:24:31 +0700

    The component roomPage changed.
    Now the component displays pictures and a component is inserted that correctly displays reviews

--- src/components/roomPage/roomPage.tsx
+++ src/components/roomPage/roomPage.tsx
@@ -1,41 +1,43 @@
-import React, { useEffect, useState, useLayoutEffect } from 'react';
+import React from 'react';
 import "./roomPage.scss";
-import { useLoaderData, useParams } from 'react-router-dom';
+import { useLoaderData } from 'react-router-dom';
 import { RoomsAPI } from '../../interfaces/roomsAPI';
 import { RoomItem } from '../../types/rooms';
 import { Carousel } from '../carousel/carousel';
 import { Field } from '../field/field';
-import { FlexContainer } from '../flexContainer/flexContainer';
-import { Form, FormFieldset } from '../form/form';
-import { Dropdown, dropButton } from '../dropdown/dropdown';
-import { Label } from '../label/label';
-import { DateMaskField } from '../dateMaskField/dateMaskField';
 import { RootState } from '../../store/reducers/rootReducer';
 import { ConnectedProps, connect } from 'react-redux';
-import { CalendarCard } from '../calendarCard/calendarCard';
-import { correctDeclensionWord } from '../correctDeclensionWord/correctDeclensionWord';
-import { NumberPicker } from '../numberPicker/numberPicker';
-import { FetchDesignations } from '../../store/actions/filterRoomsAction';
 import { FilterRoomsAPI } from '../../interfaces/FilterRoomsAPI';
 import { designations } from '../../types/filterRooms';
-import { Button } from '../button/button';
-import { FiArrowRight } from 'react-icons/fi';
 import { OrderForm } from '../orderForm/orderForm';
+import { ReviewList } from '../reviewList/reviewList';
+import { authAPI } from '../../interfaces/authAPI';
+import { userInfo } from '../../types/auth';
 
 export async function roomLoader({ params }: any) {
   const loaderData = JSON.parse(JSON.stringify(params));
   const designations: designations = await FilterRoomsAPI.FetchDesignations();
   const roomItem: RoomItem = await RoomsAPI.FetchRoomItem(loaderData.id);
+
+  if (roomItem.reviews) {
+    for (let index = 0; index < roomItem.reviews.length; index++) {
+      let authorInfo = await authAPI.FetchUserInfo(roomItem.reviews[index].authorId) as userInfo;
+      roomItem.reviews[index] = {
+        ...roomItem.reviews[index],
+        authorName: authorInfo.firstName + " " + authorInfo.lastName,
+        authorPhotoURL: authorInfo.userPhotoURL,
+      };
+    }
+  }
   return {
     designations,
     roomItem
   }
 }
+
 type RoomPageProps = {
 
-} & ConnectorProps
-  // & React.FormHTMLAttributes<HTMLFormElement>
-  ;
+} & ConnectorProps;
 
 const Page = ({ auth }: RoomPageProps) => {
   const { designations, roomItem } = useLoaderData() as {
@@ -55,10 +57,25 @@ const Page = ({ auth }: RoomPageProps) => {
   return (
     <div key={"roomPage"} className='roomPage'>
       <Carousel key={"roomPage__photos"} className='roomPage__photos' height={500} theme='big'>
-        {photos}
+        {
+          photos
+            ? photos.map((url) => {
+              return <img key={url} src={url} alt='Фото номера' />
+            })
+            : ""
+        }
       </Carousel>
       <div key={"roomPage__info"} className='roomPage__info'>
 
+      </div>
+      <div key={"roomPage__reviews"} className='roomPage__reviews'>
+        <h2 key={"header"}>
+          Отзывы посетителей номера
+        </h2>
+        {
+          reviews
+          && <ReviewList roomId={id} reviews={reviews} />
+        }
       </div>
       <Field theme='card' key={"roomPage__orderForm"}>
         <OrderForm

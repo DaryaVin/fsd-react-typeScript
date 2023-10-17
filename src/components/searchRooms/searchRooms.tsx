1986f7929f1805349ce22cb6e016a27a3b3acc2a
DaryaVin <31697627+DaryaVin@users.noreply.github.com>
2023-10-17 18:05:48 +0700

    The component searchRooms changed.
    the component is now adaptive

--- src/components/searchRooms/searchRooms.tsx
+++ src/components/searchRooms/searchRooms.tsx
@@ -1,28 +1,38 @@
-import { ref, set } from 'firebase/database';
-import React, { useEffect } from 'react';
-import { bdFirebase } from '../../firebase';
-import { RoomItem } from '../../types/rooms';
+import React, { useState } from 'react';
+import "./searchRooms.scss";
 import { FilterRoomsForm } from '../filterRoomsForm/filterRoomsForm';
-import { Pagination } from '../pagination/pagination';
-import { RoomCard } from '../roomCard/roomCard';
 import { RoomCardsList } from '../roomCardsList/roomCardsList';
 import { FilterRoomsAPI } from '../../interfaces/FilterRoomsAPI';
 import { useLoaderData } from 'react-router-dom';
 import { designations } from '../../types/filterRooms';
+import { Button } from '../button/button';
+import { GiSettingsKnobs } from 'react-icons/gi';
 
-export async function searchRoomLoader({ params }: any) {
-  const loaderData = JSON.parse(JSON.stringify(params));
+export async function searchRoomLoader() {
   const designations: designations = await FilterRoomsAPI.FetchDesignations();
   return designations;
 }
 
 export const SearchRooms = () => {
   const designations = useLoaderData() as designations;
-
+  const [isFilterShow, setIsFilterShow] = useState<boolean>(false)
   return (
     <div className='searchRooms'>
-      <FilterRoomsForm key={"FilterRoomsForm"} className="" designations={designations}></FilterRoomsForm>
-      <RoomCardsList key={"RoomCardsList"} className=''/>
+      <RoomCardsList key={"RoomCardsList"} className='searchRooms__itemList' />
+      <div className="searchRooms__filter">
+        <Button
+          type='button'
+          className={"searchRooms__filterButton"}
+          onClick={() => setIsFilterShow(!isFilterShow)}
+        >
+          <GiSettingsKnobs className="loginForm__buttonArrow" />
+          Фильтр
+        </Button>
+        <FilterRoomsForm key={"FilterRoomsForm"}
+          className={"searchRooms__filterForm" + (isFilterShow ? " searchRooms__filterForm_show" : "") }
+          designations={designations}
+        />
+      </div>
     </div>
   )
 }

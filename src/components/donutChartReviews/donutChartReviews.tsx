b5713bbe4aed338636b396b2186c432591159c1c
DaryaVin <31697627+DaryaVin@users.noreply.github.com>
2023-10-17 17:48:55 +0700

    The component donutChartReviews created.

---
+++ src/components/donutChartReviews/donutChartReviews.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import "./donutChartReviews.scss";
+import { Chart } from 'react-google-charts';
+import { ReviewItem } from '../../types/rooms';
+import { correctDeclensionWord } from '../correctDeclensionWord/correctDeclensionWord';
+
+interface DonutChartReviewsProps extends React.HTMLAttributes<HTMLDivElement> {
+  reviews: ReviewItem[],
+}
+export const DonutChartReviews = ({ reviews, ...props }: DonutChartReviewsProps) => {
+  const data = [
+    ["Оценка", "Количество отзывов"],
+    [
+      "Разочарован",
+      reviews.reduce((sum, review) => {
+        return review.appraisal === 1 || review.appraisal === 2 ? sum + 1 : sum
+      }, 0)
+    ],
+    [
+      "Удовлетворительно", 
+      reviews.reduce((sum, review) => {
+        return review.appraisal === 3 ? sum + 1 : sum
+      }, 0)
+  ],
+    [
+      "Хорошо", 
+      reviews.reduce((sum, review) => {
+        return review.appraisal === 4 ? sum + 1 : sum
+      }, 0)
+    ],
+    [
+      "Великолепно", 
+      reviews.reduce((sum, review) => {
+        return review.appraisal === 5 ? sum + 1 : sum
+      }, 0)
+  ],
+  ]
+  return (
+    <div {...props} className={'donutChartReviews' + (props.className ? " " + props.className : "")}>
+
+      <Chart
+        className="donutChartReviews__donut"
+        chartLanguage='ru-Ru'
+        height={"240px"}
+        width={"320px"}
+        chartType="PieChart"
+        loader={<div>Загрузка диаграмы отзывов...</div>}
+        data={data}
+        options={{
+          // backgroundColor: "red",
+          colors: ["919191", "BC9CFF", "6FCF97", "FFE39C"],
+          pieHole: 0.9,
+        }}
+      />
+      <div className='donutChartReviews__signature'>
+        {
+          reviews.length + " "
+          + correctDeclensionWord({
+            options: {
+              1: "голос",
+              2: "голоса",
+              5: "голосов",
+            },
+            value: reviews.length,
+          })
+        }
+      </div>
+    </div>
+  )
+}

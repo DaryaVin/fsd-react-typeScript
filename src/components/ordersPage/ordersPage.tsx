5632f5c86a177d1626ae7833489a55143a431ea0
DaryaVin <31697627+DaryaVin@users.noreply.github.com>
2023-10-30 16:57:48 +0700

    feat (ordersPage): created and chended style file for ordersPage

--- src/components/ordersPage/ordersPage.tsx
+++ src/components/ordersPage/ordersPage.tsx
@@ -1,4 +1,5 @@
 import React, { useLayoutEffect, useRef, useState, useEffect } from 'react';
+import "./ordersPage.scss";
 import { BookingCard } from '../bookingCard/bookingCard';
 import { RootState } from '../../store/reducers/rootReducer';
 import { ConnectedProps, connect } from 'react-redux';
@@ -32,7 +33,7 @@ const Page = ({ bookingState, FetchBookingsAction, ...props }: OrdersPageProps) 
               {
                 bookingState.bookings.filter((bookingItem, bookingItemIndex) => {
                   if (orderPageRef.current) {
-                    return ((bookingItemIndex + 1) % Math.floor(orderPageRef.current.clientWidth / 380)) === index - 1;
+                    return (bookingItemIndex + 1) % Math.max(Math.floor(orderPageRef.current.clientWidth / 400), 1) === index - 1;
                   }
                   return false;
                 }).map((bookingItem) => {
@@ -101,78 +102,6 @@ const connector = connect(mapStateToProps, mapDispatchToProps);
 type ConnectorProps = ConnectedProps<typeof connector>;
 export const OrdersPage = connector(Page);
 
-
-// import React, {useLayoutEffect} from 'react';
-// import { bookingAPI } from '../../interfaces/bookingAPI';
-// import { BookingCard } from '../bookingCard/bookingCard';
-// import { RoomsAPI } from '../../interfaces/roomsAPI';
-// import { RootState } from '../../store/reducers/rootReducer';
-// import { ConnectedProps, connect } from 'react-redux';
-// import {FetchBookingsAction} from "../../store/actions/bookingActions";
-
-
-// export async function ordersPageLoader() {
-//   const bookings = await bookingAPI.FetchBookings();
-//   if (bookings) {
-//     for (let index = 0; index < bookings.length; index++) {
-//       let room = await RoomsAPI.FetchRoomItem(bookings[index].roomId);
-//       bookings[index] = {
-//         ...bookings[index],
-//         isLux: room.isLux,
-//         roomName: room.name,
-//       };
-//     }
-//   }
-//   return bookings;
-// }
-
-// type OrdersPageProps = React.HTMLAttributes<HTMLDivElement> & {
-
-// } & ConnectorProps;
-// const Page = ({ bookingState, FetchBookingsAction, ...props }: OrdersPageProps) => {
-//   useLayoutEffect(() => {
-//     FetchBookingsAction();
-//   }, []);
-
-//   return (
-//     <div
-//       {...props}
-//       className={"ordersPage" + (props.className ? " " + props.className : "")}
-//     >{
-//       bookingState?.isLoading 
-//       ? "Идет загрузка ваших заказов"
-//       : bookingState?.bookings && bookingState.bookings.length !== 0
-//           ? <ul key={"bookingsList"}
-//             className='ordersPage__bookingsList'
-//           >
-//             {
-//               bookingState.bookings.map((bookingItem) => {
-//                 return <BookingCard key={bookingItem.id}
-//                   bookingItem={bookingItem}
-//                 />
-//               })
-//             }
-//           </ul>
-//           : <div>
-//             Список ваших заказов пуст
-//           </div>
-//       }
-//     </div>
-//   )
-// }
-
-// const mapStateToProps = (state: RootState) => {
-//   return ({
-//     bookingState: state.bookings
-//   })
-// }
-// const mapDispatchToProps = {
-//   FetchBookingsAction,
-// };
-
-// const connector = connect(mapStateToProps, mapDispatchToProps);
-// type ConnectorProps = ConnectedProps<typeof connector>;
-// export const OrdersPage = connector(Page);
 
 
 
